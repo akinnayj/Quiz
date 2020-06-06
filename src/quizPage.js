29ebaa8fdf704154064ba43e9273ca2f846bdd61
Annika Luu <annika-luu@hotmail.com>
2020-06-07 00:23:39 +0200

    Lagt til alternativer, testet med console.log

--- src/quizPage.js
+++ src/quizPage.js
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./App.css";
 
 function Quizzes({ kategori }) {
-  let geografi = [{Spørsmål = "Hvor bor Annika?", Alternativ="Trondheim", "Oslo", "Kristiansand", "Lillesand", riktigAlt="1"}];
+  const [count, setCount] = useState(8);
+  let geografi = [
+    {
+      spørsmål: "Hvor høyt er mt everest",
+      alternativer: [100, 200, 300, 8000],
+      riktig: 3,
+    },
+  ];
+
+  function checkAnswer(alternativ) {
+    console.log(alternativ);
+    if (alternativ == geografi[0].alternativer[geografi[0].riktig]) {
+      console.log("Correcto");
+    } else {
+      console.log("Falso");
+    }
+  }
+
   return (
     <div className="ridge">
+      <p>Spørsmål: {geografi[0].spørsmål}</p>
       <p>
-        Du valgte kategori <b>Geografi</b>
+        Det er fire alternativer:
+        <div className="alternativer">
+          {geografi[0].alternativer.map((alternativ) => (
+            <button
+              onClick={() => checkAnswer(alternativ)}
+              className="button-alternativ"
+            >
+              {alternativ}
+            </button>
+          ))}
+        </div>
+      </p>
+      <p>
+        <div className="correct"></div>
       </p>
     </div>
   );
