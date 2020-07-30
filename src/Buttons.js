15c3983786d47b193a943d10f2cbc2726a41b8b3
Annika Luu <annika-luu@hotmail.com>
2020-07-31 01:03:29 +0200

    Tested a counter in Button.js

--- src/Buttons.js
+++ src/Buttons.js
@@ -2,6 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { useHistory } from "react-router-dom";
 import "./App.css";
+import Quizzes from "./Quizzes.js";
+import Countdown from "react-countdown";
+
+function counter() {
+  const Counter = () => <span>Game over!</span>;
+
+  ReactDOM.render(
+    <Countdown date={Date.now() + 3000}>
+      <Quizzes />
+    </Countdown>,
+    document.getElementById("root")
+  );
+}
 
 function Buttons({ kategori, indexNr }) {
   const history = useHistory();
@@ -11,7 +24,11 @@ function Buttons({ kategori, indexNr }) {
   }
 
   return (
-    <button className="button-alternativ" onClick={showQuizzes}>
+    <button
+      className="button-alternativ"
+      onClick={showQuizzes}
+      onClick={counter}
+    >
       {kategori}
     </button>
   );
