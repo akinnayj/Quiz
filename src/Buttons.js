1cf7e76092d4d27718fb8c4d2c493a4c3efad382
Annika Luu <annika-luu@hotmail.com>
2020-08-11 22:11:04 +0200

    Changed to setInterval instead of react-countdown

--- src/Buttons.js
+++ src/Buttons.js
@@ -1,34 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { useHistory } from "react-router-dom";
 import "./App.css";
 import Quizzes from "./Quizzes.js";
 import Countdown from "react-countdown";
 
-function counter() {
-  const Counter = () => <span>Game over!</span>;
-
-  ReactDOM.render(
-    <Countdown date={Date.now() + 3000}>
-      <Quizzes />
-    </Countdown>,
-    document.getElementById("root")
-  );
-}
-
 function Buttons({ kategori, indexNr }) {
+  const [countdown, setCountdown] = useState(false);
   const history = useHistory();
 
   function showQuizzes() {
     history.push("/Quizzes", { category: indexNr });
   }
 
+  function timer() {}
+
+  setInterval(() => {
+    console.log("Interval triggered");
+  }, 1000);
+
   return (
-    <button
-      className="button-alternativ"
-      onClick={showQuizzes}
-      onClick={counter}
-    >
+    <button className="button-alternativ" onClick={showQuizzes}>
       {kategori}
     </button>
   );
